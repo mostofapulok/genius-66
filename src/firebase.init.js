6b1630745eb0d7e4ea5795014e444930063d1b11
mostofapulok <mostofapulok@gmail.com>
2022-05-06 00:50:27 +0600

    first commit

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_apiKey,
-  authDomain:process.env.REACT_APP_authDomain,
-  projectId:process.env.REACT_APP_projectId,
-  storageBucket:process.env.REACT_APP_storageBucket,
-  messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId:process.env.REACT_APP_appId,
+  apiKey: "AIzaSyBD4CJsPWNj2U6b4w3u45VpDjohiRLJR5s",
+  authDomain: "genius-car-65-3.firebaseapp.com",
+  projectId: "genius-car-65-3",
+  storageBucket: "genius-car-65-3.appspot.com",
+  messagingSenderId: "981775178452",
+  appId: "1:981775178452:web:6863bbe022e58afdd13941"
 };
 
 // Initialize Firebase
